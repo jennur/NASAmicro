b1dc98b25483fb040c319476d55b3202c53b3242
jennur <jenny.bonsak@gmail.com>
2018-06-01 19:51:07 +0200

    Added scroll and hover-circle functionality

---
+++ scripts/home.js
@@ -0,0 +1,24 @@
+/*
+window.onscroll = function(){
+  var asteroid = document.getElementById('top-container');
+  var discover = document.getElementById('bottom-container');
+  asteroid.style.opacity = 1 - (window.pageYOffset)/600;
+  discover.style.opacity = (window.pageYOffset + 65)/600;
+}
+*/
+
+
+/*Hover function for circle-element
+
+var bgCircle = document.getElementById('bg-circle');
+var bgExplContainer = document.getElementById('bg-expl-container');
+var bgBox = document.getElementById('bg-box');
+
+bgCircle.onmouseover = function(){
+  bgBox.style.display = "block";
+
+}
+bgExplContainer.onmouseleave = function(){
+  bgBox.style.display = "none";
+}
+*/

076f7abf21a8de3e3d207dec98fc7d38bd5a3b07
jennur <jenny.bonsak@gmail.com>
2018-06-03 05:49:57 +0200

    Revert "Revert "Added background functionality""
    This reverts commit 4138ce1aa2c4e67aa10c64f4a23fdcbae3e86d02.

--- scripts/api.js
+++ scripts/api.js
@@ -8,11 +8,34 @@ var endDate = startDate;//dateFormat(nextDate);
 console.log(startDate);
 
 var request = new XMLHttpRequest(); //creating an HTTP request to connect to the API
-request.open('GET', 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + startDate + '&end_date=' + endDate + '&detailed=true&api_key=' + apiKey, true); //making asynchronous call to load JSON data
+request.open('GET', 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + startDate + '&end_date=' + endDate + '&detailed=true&api_key=' + apiKey, true);
+//request.open('GET', 'https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key=' + apiKey, true); //making asynchronous call to load JSON data
 request.onload = function(){ //creating function to be called when content is loaded
   if(this.status >= 200 && this.status < 400){ //if the request was successful
     var data = JSON.parse(this.response); //assign loaded JSON content to variable
     console.log(data);
+    console.log();
+
+    var asteroidName = document.getElementById('asteroid-name');
+    asteroidName.innerHTML = data["near_earth_objects"][startDate]["0"]["name"];
+
+    //Append content to page
+    var details = document.getElementById('details-list');
+
+    var asteroid = data["near_earth_objects"][startDate]["0"];
+    var diameter = document.createElement('li');
+    details.appendChild(diameter);
+    diameter.innerHTML = "<b>Estimated diameter (min/max):</b><br /> " + asteroid.estimated_diameter.meters.estimated_diameter_min + " m / "
+    + asteroid.estimated_diameter.meters.estimated_diameter_max + " m <br />"
+    + asteroid.estimated_diameter.feet.estimated_diameter_min + " ft / " + asteroid.estimated_diameter.feet.estimated_diameter_max + " ft";
+
+    var velocity = document.createElement('li');
+    details.append(velocity);
+    velocity.innerHTML = "<b>Relative velocity:</b><br />" + asteroid.close_approach_data["0"].relative_velocity.kilometers_per_hour + " km/h <br />"
+    + asteroid.close_approach_data["0"].relative_velocity.miles_per_hour + "miles/h";
+
+    var distance = document.createElement('li');
+    details.appendChild(distance);
   }
   else{
     console.log('error. Request status: ' + request.status); //error message to the console if request failed
